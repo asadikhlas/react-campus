13c5675a53529e959458d942eda6b1efbbd51026
asadikhlas <asadikhlas03@gmail.com>
2019-05-02 15:50:00 +0500

    add routers

--- client/src/Components/Routers.js
+++ client/src/Components/Routers.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import {Route, BrowserRouter as Router} from 'react-router-dom';
 
 class Routers extends Component {
   render() {
     return (
-      <div>
-        hello from routers
-      </div>
+        <Router>
+            <div>
+                <Route exact path="/" component={SignIn} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/admindashboard" component={AdminDashboard} />
+                <Route exact path="/studentdashboard" component={StudentDashboard} />
+                <Route exact path="/companydashboard" component={CompanyDashboard} />
+                <Route exact path="/createjob" component={PostJob} />
+            </div>
+        </Router>
     )
   }
 }
