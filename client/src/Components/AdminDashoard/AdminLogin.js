5ee4b5e18542852a50e38f4637157f44b87c3ad5
asadikhlas <asadikhlas03@gmail.com>
2019-05-09 14:19:31 +0500

    working

--- client/src/Components/AdminDashoard/AdminLogin.js
+++ client/src/Components/AdminDashoard/AdminLogin.js
@@ -7,13 +7,13 @@ import {
   Header,
   Message
 } from "semantic-ui-react";
-import {connect} from 'react-redux';
-import {adminAsyncLogin} from '../../Store/Middlewares/adminLoginMiddleware';
+import { connect } from "react-redux";
+import { adminAsyncLogin } from "../../Store/Middlewares/adminLoginMiddleware";
 
 class AdminLogin extends Component {
   state = {
     email: "",
-    password: "",
+    password: ""
   };
   handleChange = event => {
     this.setState({
@@ -21,34 +21,32 @@ class AdminLogin extends Component {
     });
   };
 
-  handleSubmit = (event) => {
-    event.preventDefault()
-    const {email, password} = this.state
+  handleSubmit = event => {
+    event.preventDefault();
+    const { email, password } = this.state;
     const newObj = {
       email,
       password
-    }
+    };
 
-    this.props.adminAsyncLogin(newObj)
-
-   }
-
+    this.props.adminAsyncLogin(newObj);
+  };
 
   componentWillReceiveProps(newProps) {
     const oldProps = this.props;
     if (oldProps.adminUser !== newProps.adminUser) {
       this.props.history.push("/admindashboard");
-    }else{
-      this.props.history.push("/adminlogin")
+    } else {
+      this.props.history.push("/adminlogin");
     }
   }
- 
+
   render() {
     return (
       <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxWidth: 1000 }}>
           <Header as="h2" icon color="violet" textAlign="center">
-           Admin Login Here
+            Admin Login Here
           </Header>
           <Form
             onSubmit={this.handleSubmit}
@@ -92,20 +90,22 @@ class AdminLogin extends Component {
   }
 }
 
-const mapStateToProps = ({adminLoginReducer}) => {
+const mapStateToProps = ({ adminLoginReducer }) => {
   return {
     adminUser: adminLoginReducer.adminUser,
     errorMessage: adminLoginReducer.errorMessage
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    adminAsyncLogin: (data) => {
-      dispatch(adminAsyncLogin(data))
+    adminAsyncLogin: data => {
+      dispatch(adminAsyncLogin(data));
     }
-  }
-}
+  };
+};
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(AdminLogin);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AdminLogin);
