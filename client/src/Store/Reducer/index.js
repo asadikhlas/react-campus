97d80b93909f9586bc8455ca344a6fa01240a1a2
asadikhlas <asadikhlas03@gmail.com>
2019-05-09 13:24:45 +0500

    working

--- client/src/Store/Reducer/index.js
+++ client/src/Store/Reducer/index.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux';
 import {loginReducer} from './login_reducer'
 import { companyLoginReducer } from './companyLoginReducer';
+import {adminLoginReducer} from './adminLoginReducer'
 
 
 export const rootReducer = combineReducers({
     loginReducer,
-    companyLoginReducer
+    companyLoginReducer,
+    adminLoginReducer
 })
