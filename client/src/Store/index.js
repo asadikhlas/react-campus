643e900db991b7ef92a0aa6681a5428cfffce196
asadikhlas <asadikhlas03@gmail.com>
2019-05-11 15:09:55 +0500

    added redux persist

--- client/src/Store/index.js
+++ client/src/Store/index.js
@@ -1,10 +1,18 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import { rootReducer } from "./Reducer";
 import thunk from "redux-thunk";
+import {persistReducer, persistStore} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = { 
+  key: 'root', 
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
+export const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(thunk)))
+export const persistor = persistStore(store)
+
--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./Store";
+import {store, persistor} from "./Store";
 import "semantic-ui-css/semantic.min.css";
+import { PersistGate } from 'redux-persist/integration/react'
+
+
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading = {null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
 
   document.getElementById("root")
