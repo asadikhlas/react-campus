987c614c6df0dd80bcf996f42f84c7642b44c7a7
asadikhlas <asadikhlas03@gmail.com>
2019-05-11 16:56:19 +0500

    completed

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-import {store, persistor} from "./Store";
+import { store, persistor } from "./Store";
 import "semantic-ui-css/semantic.min.css";
-import { PersistGate } from 'redux-persist/integration/react'
-
-
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate loading = {null} persistor={persistor}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
     </PersistGate>
   </Provider>,
